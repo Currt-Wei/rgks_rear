d22fe7dcde9745579e8b57aad74dc093cb8a3163
韦家焕 <weijiahuan.curry@bytedance.com>
2021-05-05 15:24:39 +0800

    init rgks

---
+++ src/main/java/com/example/rgks_rear/RgksRearApplication.java
@@ -0,0 +1,13 @@
+package com.example.rgks_rear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgksRearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RgksRearApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rgks_rear/RgksRearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rgks_rear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RgksRearApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
