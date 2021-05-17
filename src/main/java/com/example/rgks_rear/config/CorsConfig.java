2af41dab368621c19c65b39bce8747a710aaf8fc
韦家焕 <2698230239@qq.com>
2021-05-17 15:57:19 +0800

    跨域请求

---
+++ src/main/java/com/example/rgks_rear/config/CorsConfig.java
@@ -0,0 +1,16 @@
+package com.example.rgks_rear.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    static final String ORIGINS[] = new String[] { "GET", "POST", "PUT", "DELETE" };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowCredentials(true).allowedMethods(ORIGINS).maxAge(3600);
+    }
+
+}
--- src/main/java/com/example/rgks_rear/config/WebConfig.java
+++ src/main/java/com/example/rgks_rear/config/WebConfig.java
@@ -16,5 +16,4 @@ public class WebConfig implements WebMvcConfigurer {
 //    public void addResourceHandlers(ResourceHandlerRegistry registry) {
 //        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
 //    }
-
 }
