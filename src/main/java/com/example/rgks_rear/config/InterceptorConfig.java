7f80793d18498371505b7ccd12796ae7eb139a81
李泓锦 <lihongjin.happy@bytedance.com>
2021-06-16 14:16:26 +0800

    fix:去掉查询token

--- src/main/java/com/example/rgks_rear/config/InterceptorConfig.java
+++ src/main/java/com/example/rgks_rear/config/InterceptorConfig.java
@@ -14,7 +14,8 @@ public class InterceptorConfig extends WebMvcConfigurationSupport {
 
     public void addInterceptors(InterceptorRegistry registry) {
         //此处配置拦截路径
-        registry.addInterceptor(paramInterceptor).addPathPatterns("/**").excludePathPatterns("/index/*");
+        registry.addInterceptor(paramInterceptor).addPathPatterns("/**").excludePathPatterns("/index/*")
+                .excludePathPatterns("/exercise/query");
     }
 
 }
